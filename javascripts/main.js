9eea169c3b1157024c18b0cc53ad39396b3fe44c
govind <govind@valuebound.com>
2019-12-29 13:27:38 +0530

    this is new function for uploading file

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+  var file = document.getElementById("file");
+
+  file.addEventListener(onchange, e => {
+    console.log("govind");
+    console.log(e);
+  });
+};
+
+// // Listen for form submit
+// form.addEventListener("file", e => {
+//   e.preventDefault();
+//   var file = document.querySelector("[type=file]").file;
+//   const formData = new FormData();
+//   formData.append("file", file);
+//   console.log(formData);
+
+//   // ...
+// });
+
+// function compute(e) {
+//   console.log(e);
+
+//   console.log("compute function");
+// }
